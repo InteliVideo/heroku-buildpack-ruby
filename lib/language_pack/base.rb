ffa314c450e5811cbe919918ae16cfe987139149
CJ <cjgrimes@gmail.com>
2023-05-24 12:17:55 -0600

    update ruby download url

--- lib/language_pack/base.rb
+++ lib/language_pack/base.rb
@@ -17,7 +17,7 @@ class LanguagePack::Base
   include LanguagePack::ShellHelpers
   extend LanguagePack::ShellHelpers
 
-  VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://heroku-buildpack-ruby.s3.us-east-1.amazonaws.com"
+  VENDOR_URL           = ENV['BUILDPACK_VENDOR_URL'] || "https://cache.ruby-lang.org/pub/ruby/2.4/ruby-2.4.10.tar.gz"
   DEFAULT_LEGACY_STACK = "cedar"
   ROOT_DIR             = File.expand_path("../../..", __FILE__)
 
